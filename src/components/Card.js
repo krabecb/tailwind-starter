13e9b18faa2eada88cf540598ee6cb375c62edce
krabecb <briankrabec@gmail.com>
2023-05-04 23:44:20 -0500

    Starter

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,9 @@
 export default function Card({data}) {
     return (
-        <div className="bg-slate-50 w-3/4 p-1.5 rounded flex flex-col items-center mx-auto mb-3">
-            <h3 className="font-bold">{data.title}</h3>
-            <img src={data.image} className="h-auto w-1/4 rounded-md"/>
-            <h4 className="font-medium">${data.price}</h4>
+        <div className="">
+            <h3 className="">{data.title}</h3>
+            <img src={data.image} className=""/>
+            <h4 className="">${data.price}</h4>
             <p>{data.description}</p>
         </div>
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import image from '../images/brad-preece-MRxx3VJPho4-unsplash.jpg'
+
 export default function Header() {
     return (
-        <div className="flex flex-col items-center">
-            <h1 className='font-mono text-xl m-2.5'>My Bowling Alley Business</h1>
-            <img className='h-auto w-1/4 rounded-md flex justify-center mb-7 hover: focus' src={image} />
+        <div className="">
+            <h1 className=''>My Bowling Alley Business</h1>
+            <img className='' src={image} />
         </div>
     )
 }
